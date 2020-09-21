d5fc476b998d0853318ad2039ecccf000a595e4e
ArthurGoupil <arthurgoupil@gmail.com>
2020-09-21 11:50:56 +0200

    add tooltip with some issues

--- src/components/MyEditor.js
+++ src/components/MyEditor.js
@@ -25,12 +25,44 @@ function MyEditor() {
     EditorState.createEmpty()
   );
   const [savedSelections, setSavedSelections] = useState([]);
+  const [displayTooltip, setDisplayTooltip] = useState(false);
+  const [windowSelectionDOMNodeData, setWindowSelectionDOMNodeData] = useState(
+    null
+  );
 
   const contentState = editorState.getCurrentContent();
+
+  // get selection with window element
+  const getWindowSelection = () => {
+    let windowSelection;
+    if (window.getSelection) {
+      windowSelection = window.getSelection();
+    } else if (document.getSelection) {
+      windowSelection = document.getSelection();
+    } else if (document.selection) {
+      windowSelection = document.selection.createRange().text;
+    }
+    return windowSelection;
+  };
 
   const handleEditorChange = (newEditorState) => {
     const content = editorState.getCurrentContent();
     const newContent = newEditorState.getCurrentContent();
+
+    const selection = newEditorState.getSelection();
+
+    // retrieve window selection DOM node data for tooltip positioning
+    // first part of condition : the selection exists ? second part: avoid IndexSizeError
+    if (!selection.isCollapsed() && window.getSelection().rangeCount > 0) {
+      const windowSelection = getWindowSelection();
+      const DOMNodeData = windowSelection.getRangeAt(0).getBoundingClientRect();
+      setWindowSelectionDOMNodeData(DOMNodeData);
+      setDisplayTooltip(true);
+      console.log(DOMNodeData);
+    } else {
+      setDisplayTooltip(false);
+      setWindowSelectionDOMNodeData(null);
+    }
 
     setEditorState(newEditorState);
   };
@@ -158,6 +190,15 @@ function MyEditor() {
     },
   };
 
+  const getTooltipData = () => {
+    if (windowSelectionDOMNodeData) {
+      return {
+        x: windowSelectionDOMNodeData.y + window.pageYOffset - 58,
+        y: windowSelectionDOMNodeData.x + window.pageXOffset - 8,
+      };
+    } else return { x: 0, y: 0 };
+  };
+
   return (
     <div className='editor-container'>
       <BlockStyleToolbar
@@ -205,6 +246,37 @@ function MyEditor() {
       <div>
         Current selection is: <b>{getCurrentSelection(editorState)}</b>
       </div>
+      {displayTooltip && (
+        <>
+          <div className='tooltip-test' />
+          <style>
+            {`
+          .tooltip-test {
+            position: absolute;
+            background-color: white;
+            border-radius: 5px;
+            top: ${getTooltipData().x}px;
+            left: ${getTooltipData().y}px;
+            width: 200px;
+            height: 50px;
+            z-index: 2;
+            filter: drop-shadow(0px 0px 20px rgba(0, 0, 0, 0.3));
+          }
+          .tooltip-test::before {
+            content: '';
+            position: absolute;
+            transform: rotate(45deg);
+            background-color: white;
+            width: 8px;
+            height: 8px;
+            left: 8px;
+            bottom: -4px;
+            z-index: -1;
+          }
+        `}
+          </style>
+        </>
+      )}
     </div>
   );
 }
